d50d3e15906f583b77846c78a8371c46561d9581
Lucas <us3.lucas@gmail.com>
2025-04-17 22:08:06 -0300

    GradeEstudante.java

--- GradeEstudante.java
+++ GradeEstudante.java
@@ -1,21 +1,32 @@
+import java.util.Locale; 
+import java.util.Scanner; 
 
-public class Student {
+public class Main { 
     
-    public String name;
-    public double grade1;
-    public double grade2;
-    public double grade3;
+    public static void main(String[] args) { 
+        
+        Locale.setDefault(Locale.US); 
+        Scanner sc = new Scanner(System.in); 
 
-    public double finalGrade() {
-        return grade1 + grade2 + grade3;
-    }
+        Student student = new Student();
 
-    public double missingPoints() {
-        if (finalGrade() < 60.0) {
-            return 60.0 - finalGrade();
-        } 
-        else {
-            return 0.0;
+        System.out.print("Name: ");
+        student.name = sc.nextLine();
+        System.out.print("Grade 1: ");
+        student.grade1 = sc.nextDouble();
+        System.out.print("Grade 2: ");
+        student.grade2 = sc.nextDouble();
+        System.out.print("Grade 3: ");
+        student.grade3 = sc.nextDouble();
+
+        System.out.printf("FINAL GRADE: %.2f%n", student.finalGrade());
+        
+        if (student.finalGrade() < 60.0) {
+            System.out.println("FAILED");
+            System.out.printf("MISSING %.2f POINTS%n", student.missingPoints());
+        } else { 
+            System.out.println("PASS");
         }
-    }
+        sc.close();
+    } 
 }
